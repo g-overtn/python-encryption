fee1e6001ec251d5551c9274a316ed646734c3f5
George Overton <george.overton69@gmail.com>
2025-10-02 18:48:45 +0100

    Create caesar cipher.py

---
+++ caesar cipher.py
@@ -0,0 +1,29 @@
+def CaesarCipher(plaintext : str, key : int, direction : int) -> str:
+    """
+    Performs a Caesar Cipher encryption/decryption algorithm on a given string, converts input to lowercase.
+
+    Parameters:
+        plaintext (str): the given string to be encrypted or decrypted.
+        key (int): the key used to encrypt or decrypt the message.
+        direction (int): decides whether to encrypt or decrypt the message. 1: encrypt, -1: decrypt.
+
+    Returns:
+        output (str): the encrypted/decrypted string.
+    """
+
+    output : str = ""
+    alphabet : set = set("abcdefghijklmnopqrstuvwxyz")
+    
+    for c in plaintext.lower():
+        if c in alphabet:
+            val = (ord(c) - 96) + (key * direction)
+            if val < 1: val += 26
+            elif val > 26: val -= 26
+            output += chr(val + 96)
+        else:
+            output += c
+
+    return output
+
+print(CaesarCipher("The quick brown fox jumps over the lazy dog.", 23, 1))
+print(CaesarCipher("qeb nrfzh yoltk clu grjmp lsbo qeb ixwv ald.", 23, -1))
