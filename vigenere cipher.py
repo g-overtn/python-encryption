f30148c030c9188fc03dad870aef1fb3a62139ae
George Overton <george.overton69@gmail.com>
2025-10-02 19:33:25 +0100

    Update vigenere cipher.py

--- vigenere cipher.py
+++ vigenere cipher.py
@@ -1,6 +1,6 @@
 def VigenereCipher(plaintext : str, key : str, direction : int) -> str:
     """
-    Performs a Vigenere Cipher encryption/decryption algorithm on a given string, converts input to lowercase and removes punctuation.
+    Performs a Vigenere Cipher encryption/decryption algorithm on a given string, converts input to lowercase and removes all punctuation.
 
     Parameters:
         plaintext (str): the given string to be encrypted or decrypted.
@@ -12,7 +12,23 @@ def VigenereCipher(plaintext : str, key : str, direction : int) -> str:
     """
 
     ciphertext = ""
+    keystream = ""
+    alphabet : set = set("abcdefghijklmnopqrstuvwxyz")
+    plaintext = "".join(c for c in plaintext.lower() if c in alphabet)
+
+    #extend/shrink key to match plaintext length
+    for i in range(len(plaintext) // len(key)):
+        keystream += key
+    keystream += key[:len(plaintext) % len(key)]
+
+    for i in range(len(plaintext)):
+        val = (ord(plaintext[i]) - 96) + ((ord(keystream[i]) - 96) * direction)
+        if val < 1: val += 26
+        elif val > 26: val -= 26
+
+        ciphertext += chr(val + 96)
 
     return ciphertext
 
-print(VigenereCipher("attack at dawn", "lemon", 1))
+print(VigenereCipher("Attack at dawn!", "lemon", 1))
+print(VigenereCipher("mygpqwfgsois", "lemon", -1))
