7a714090a7e2c146796a94cc485e12224ee277fb
George Overton <george.overton69@gmail.com>
2025-11-18 13:31:38 +0000

    Finished nihilist decipher

--- nihilist_cipher.py
+++ nihilist_cipher.py
@@ -22,7 +22,8 @@ def constructPolybius(key : str) -> list[str]:
 
 def NihilistEncipher(plaintext : str, key1 : str, key2: str) -> str:
     """
-    Performs a Nihilist enciphering algorithm on the given string, using two given keys. Converts to lowercase and removes punctuation.
+    Performs a Nihilist enciphering algorithm on the given string, using two given keys.
+    Converts string and keys into lowercase and removes punctuation.
 
     Parameters:
         plaintext (str): the string to encipher.
@@ -35,6 +36,8 @@ def NihilistEncipher(plaintext : str, key1 : str, key2: str) -> str:
     ciphertext : str = ""
     alphabet : list = list("abcdefghijklmnopqrstuvwxyz")
     plaintext = "".join(c for c in plaintext.lower() if c in set(alphabet))
+    key1 = "".join(c for c in key1.lower() if c in set(alphabet))
+    key2 = "".join(c for c in key2.lower() if c in set(alphabet))
 
     ps : list[str] = constructPolybius(key1.lower())
 
@@ -73,8 +76,53 @@ def NihilistEncipher(plaintext : str, key1 : str, key2: str) -> str:
 
     #add up and concatenate to ciphertext
     for i in range(n):
-        ciphertext += " " + str(nums[i] + key_nums[i])
+        ciphertext += str(nums[i] + key_nums[i]) + " "
 
     return ciphertext.rstrip()
 
+def NihilistDecipher(ciphertext : str, key1 : str, key2 : str) -> str:
+    """
+    Performs a nihilist deciphering algorithm on a given string of numbers, using two keys.
+    Converts keys into lowercase and removes punctuation.
+    As 'i' and 'j' share the same square, all j's in the original text are converted into i's.
+
+    Parameters:
+        ciphertext (str): the text to decipher.
+        key1 (str): the key used in constructing a polybius square.
+        key2 (str): the key that is subtracted from the enciphered string.
+
+    Returns:
+        plaintext (str): the deciphered text.
+    """
+    plaintext : str = ""
+    alphabet : set = set("abcdefghijklmnopqrstuvwxyz")
+    key1 = "".join(c for c in key1.lower() if c in alphabet)
+    key2 = "".join(c for c in key2.lower() if c in alphabet)
+
+    ps : list[str] = constructPolybius(key1.lower())
+    ciphertext = ciphertext.split(" ") #split string into a list of numbers
+
+    #shrink/extend key2 and convert into coordinates
+    n = len(ciphertext)
+    keystream : str = ""
+    for i in range(n // len(key2)):
+        keystream += key2
+    keystream += key2[:n % len(key2)]
+
+    key_nums : list[int] = []
+    for char in keystream:
+        for i in range(5):
+            if char in ps[i]:
+                key_nums.append(int(str(i+1) + str(ps[i].index(char) + 1)))
+    
+    #subtract key_nums from ciphertext to generate original coordinates
+    ciphertext = [int(ciphertext[i]) - key_nums[i] for i in range(n)]
+
+    #convert coordinate into character and append to plaintext
+    for num in ciphertext:
+        plaintext += ps[(num // 10) - 1][(num % 10) - 1]
+
+    return plaintext
+
 print(NihilistEncipher("Dynamite Winter Palace!", "zebras", "russian"))
+print(NihilistDecipher("37 106 62 36 67 47 86 26 104 53 62 77 27 55 57 66 55 36 54 27", "zebras", "russian"))
